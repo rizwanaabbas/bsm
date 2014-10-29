e81de7c7baa63df3352b3373f6efceed3f286a2e
Rizwan Abbas <rizwanaabbas@gmail.com>
2014-10-30 00:50:24 +0500

    First commit

---
+++ app/controller.js
@@ -0,0 +1,171 @@
+/**
+ * Controller class
+ *
+ * Responsible for: 
+ *  - Initial setup and configuration
+ *  - The main loop (fetching/processing images)
+ *  - Handling user input, switch between views
+ *
+ * TODO: Move all traces of DOM access to view.js. 
+ */
+function Controller()
+{
+	var that = this;
+
+	// Objects
+	this.view = new View(this);
+	this.cameras = [];
+	
+	var initialize = function()
+	{
+		// Callback to handle JSON 
+		var processJson = function(json)
+		{
+			for(var i in json.cameras) {
+				var cam = new Camera(json.cameras[i]);
+				cam.id = i;
+				cam.controller = that; // FIXME: Doesn't need controller!
+				that.cameras.push(cam);
+			}
+			// Intitialize view and begin requesting. 
+			that.view.initCameraDom();
+			that.sizeWindow();
+			that.mainLoop();
+		};
+
+		// Load camera data. 
+		//$.getJSON('./mycameras.json', processJson);
+		//$.getJSON('./example.json', processJson);
+                $.getJSON('http://madebypakistan.com/projects/server/getdata.php?usertoken='+localStorage.usertoken, processJson);
+		
+		//$.getJSON('http://madebypakistan.com/projects/server/getdata.php?usertoken='+readCookie('usertoken'), processJson);
+		
+		// Install callbacks. 
+		$('#options').change(function(){ that.changeOptions(); });
+		$(window).resize(function() { that.sizeWindow(); });
+	};
+
+	initialize();
+
+	/**
+	 * Main loop controls image loading, etc.
+	 * FIXME: Should be private or toggleable/only callable once. 
+	 * XXX: Only called once, by constructor!
+	 */
+	this.mainLoop = function()
+	{
+		var that = this;
+		var now = (new Date()).getTime();
+		var cam = null;
+
+		//print(navigator.userAgent);
+
+		// Purge old items from the queue.
+		// This only occurs when a successful hit is made. 
+		for(var i = 0; i < this.cameras.length; i++) {
+			cam = this.cameras[i];
+			cam.queue.purgeOld();
+		}
+
+		// Perform requests. 
+		// Requests are throttled by queue size and can be paused entirely.
+		for(var i = 0; i < this.cameras.length; i++)
+		{
+			cam = this.cameras[i];
+
+			if(cam.isPaused) {
+				continue;
+			}
+
+			// Throttle requests by the size of the queue. 
+			cam.throttle = Math.round(7000 * cam.queue.percentFull());
+			if(cam.queue.newest() + cam.throttle > now) {
+				continue;
+			}
+
+			cam.queue.request(cam.url.getImageUrl());
+		}
+
+		setTimeout(function() { that.mainLoop(); }, 800);
+	};
+
+	/**
+	 * Update state to match user preference. (A callback)
+	 * 
+	 * User toggles options on form, and this callback updates all 
+	 * parameters: (1) source of the images as either remote/internet or
+	 * local/lan, (2) number or images displayed per row in the UI.
+	 */
+	this.changeOptions = function()
+	{
+		var local;
+
+		// Set cameras as local or remote. 
+		local = (this.view.getOptionServer() === 'local')? true : false;
+		for(var i = 0; i < this.cameras.length; i++) {
+			if(local) {
+				this.cameras[i].url.setLocal();
+			}
+			else {
+				this.cameras[i].url.setRemote();
+			}
+		}
+
+		// Change row width. 
+		this.view.multiviewImagesPerRow = this.view.getOptionRowWidth();
+
+		// Adjust images to window width.  
+		// XXX: Do twice to ensure it sets. (Sometimes glitches out.)
+		this.sizeWindow();
+		this.sizeWindow();
+	};
+
+	/**
+	 * Resize the image blocks to fit the window. Fits multiple images 
+	 * per row, as configured. (TODO: Belongs in view.)
+	 */
+	this.sizeWindow = function()
+	{
+		var width = $(window).width();
+		var height = $(window).height();
+                //alert(width);
+		//var NUM = this.view.multiviewImagesPerRow;
+		//var newImgWidth = Math.floor(width/NUM);
+                if(width<340)
+                {    
+                    var newImgWidth = Math.floor(width/1);
+                    var newImgHeight = Math.floor(newImgWidth/640 * 480);
+                }
+                else
+                {
+                    var newImgWidth = Math.floor(width/2);
+                    var newImgHeight = Math.floor(newImgWidth/640 * 480);
+                }    
+                    
+		var resize = function() {
+			this.width = newImgWidth;
+			this.height = newImgHeight;
+		};
+		// Resize image and outer div. 
+		$('img').each(resize);
+		$('.multiview_cam').width(newImgWidth);
+                //$('.multiview_cam').width('100%');
+    
+		// Resize singleview images.
+		var SINGLE_SCALE = 0.60;
+                $('.singleview_cam img').width('100%');
+		$('.singleview_cam img').height('100%');
+                //$('.singleview_cam img').width(Math.floor(width * SINGLE_SCALE));
+		//$('.singleview_cam img').height(Math.floor((width * SINGLE_SCALE)/640 * 480));
+
+		//$('div.singleview_stats').width(Math.floor(width * (1.0 - SINGLE_SCALE)));
+	};
+};
+
+/**
+ * For debugging.
+ */
+function print(text)
+{
+	$('#main').prepend('<h1>' + text + '</h1>');
+};
---
+++ app/view.js
@@ -0,0 +1,253 @@
+/**
+ * View class
+ * Manipulates the page DOM under instructions from the Controller.
+ * Responsible for initial setup and DOM state changes.
+ */
+function View(controller)
+{
+	// Object references.
+	this.controller = typeof(controller) != 'undefined' ? controller : null;
+	this.cameras = typeof(controller) != 'undefined'? controller.cameras : [];
+
+	// Default view values
+	this.multiviewImagesPerRow = 2;
+
+	// Selector prototypes. Append 'id' to select individual cams.
+	this.MULTIVIEW_IMAGE_SELECTOR = '#main #multiview_cam_';
+	this.MULTIVIEW_TITLE_SELECTOR = '#main #multiview_title_';
+	this.SINGLEVIEW_IMAGE_SELECTOR = '#main #singleview_cam_';
+	this.SINGLEVIEW_STATS_SELECTOR = '#main #singleview_stats_';
+
+	/**
+	 * Setup the DOM for the cameras.
+	 * Necessary to perform once camera JSON is loaded, and once again
+	 * if a new set of cameras is to replace them. 
+	 */
+	this.initCameraDom = function()
+	{
+		var that = this;
+		var defaultImage = './app/img/unavailable.png';
+
+		// Load cameras. (FIXME: Poor form)
+		this.cameras = this.controller.cameras;
+
+		// If SVG support, use SVG default image.
+		if(document.implementation.hasFeature(
+			"http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")) {
+				defaultImage = './app/img/unavailable.svg';
+		}
+
+		// Mobile firefox doesn't resize SVG correctly.
+		// FIXME: Pathetic fix. 
+		if(navigator.userAgent.search('Mozilla.*Android') > -1) {
+			defaultImage = './app/img/unavailable.png';
+		}
+
+		// Prevent "page loading..." icon from persisting indefinitely.
+		// XXX: Be careful! If any other media remains to load, it won't!
+		// XXX/NOTE: Does not seem to be the cause of Android's browser
+		// weird behavior wrt images. Firefox mobile works fine. 
+		var temp = new Image();
+		temp.onload = function(){ window.stop(); };
+		temp.src = defaultImage;
+
+		// Create templates for each camera
+		for(var i = 0; i < this.cameras.length; i++)
+		{
+			var cam = this.cameras[i];
+			var camSelector = this.MULTIVIEW_IMAGE_SELECTOR + i;
+			var singleSelector = this.SINGLEVIEW_IMAGE_SELECTOR + i;
+			var data = {};
+			var tpl;
+			var item;
+	
+			// Lazy copy the camera keys. 
+			for(var k in cam) {
+				data[k] = cam[k];
+			}
+			data['id'] = i;
+			data['camDefaultImage'] = defaultImage;
+
+			// Attach image
+			tpl = $('#multiviewTemplate').tmpl(data);
+			tpl.appendTo('#main');
+
+			// Prepend image titlebar
+			tpl = $('#multiviewTitleTemplate').tmpl(data);
+			tpl.prependTo(camSelector);
+
+			// Singleview templates.
+			tpl = $('#singleviewTemplate').tmpl(data);
+			tpl.appendTo('#main');
+			tpl.hide();
+
+			// Prepend singleview stats. 
+			tpl = $('#singleviewStatsTemplate').tmpl(data);
+			tpl.prependTo(singleSelector);
+			tpl.hide();
+
+			// Install callbacks.
+			$(".singleview_cam").click(function(){ that.multiview(); });
+			$(camSelector).mousedown((function(cam) {
+				return function(ev) { that.clickMultiviewCam(cam, ev); };
+			})(cam));
+	
+			// Disable right click menu on multiview camera images
+			$(camSelector).bind('contextmenu', function(){ return false; });
+		}
+	}
+
+	this.clickMultiviewCam = function(cam, ev)
+	{
+		switch(ev.which) {
+			case 1: // Right click
+				this.singleview(cam);
+				break;
+			case 3: // Left click
+				cam.togglePause();
+				break;
+		}
+	}
+
+	/**
+	 * Callback used by a refreshing camera after the latest frame
+	 * loads in order to update its image, statistics, etc.
+	 * Input: Camera object. 
+	 * FIXME: Terrible, unorganized mess. 
+	 */
+	this.updateCameraView = function(cam)
+	{
+		var node;
+		var tpl;
+		var mSelector = this.MULTIVIEW_IMAGE_SELECTOR + cam.id;
+		var sSelector = this.SINGLEVIEW_IMAGE_SELECTOR + cam.id;
+
+		// Title formatted times. 
+		var getTime = function(time, seconds) 
+		{
+			var d = new Date(time);
+			var s = "";
+
+			if(!time) {
+				return " &mdash; ";
+			}
+
+			s += d.getHours() + ":";
+			s += (d.getMinutes() < 10)? "0" + d.getMinutes() : d.getMinutes(); 
+
+			if(seconds) {
+				s+= ":";
+				s+= (d.getSeconds()<10)? "0" + d.getSeconds() : d.getSeconds();
+			}
+
+			delete d;
+			return s;
+		}
+
+		// Update image
+		if(cam.curFrame.src) {
+			$(mSelector + ' img').attr('src', cam.curFrame.src);
+			$(sSelector + ' img').attr('src', cam.curFrame.src);
+		}
+
+		// Update image titlebar (name, a few stats, ...)
+		node = $(this.MULTIVIEW_TITLE_SELECTOR + cam.id);
+
+		tpl = node.tmplItem();
+		tpl.data['cameraName'] = cam.location;
+		tpl.data['timeLastLoaded'] = getTime(cam.times.lastLoaded);
+		var percent = cam.queue? cam.queue.percentFull()*100 : 0;
+		tpl.data['queuePercent'] = percent.toFixed(1);
+
+		var fps = ((new Date()).getTime() - 
+				cam.times.firstRequested) / 1000;
+		tpl.data['fps'] = (cam.counts.load / fps).toFixed(3);
+		tpl.update();
+
+		// Update singleview stats
+		node = $(this.SINGLEVIEW_STATS_SELECTOR + cam.id);
+
+		tpl = node.tmplItem();
+
+		// Lazy copy dictionary
+		for(var k in cam) {
+			tpl.data[k] = cam[k];
+		}
+
+		// TODO: Report queue stats, FPS, delta times, etc. Also include title.
+		tpl.data['timeFirstRequested'] = 
+			getTime(cam.times.firstRequested, 1);
+		tpl.data['timeFirstLoaded'] = getTime(cam.times.firstLoaded, 1);
+		tpl.data['timeLastRequested'] = getTime(cam.times.lastRequested, 1);
+		tpl.data['timeLastLoadedRequested'] = 
+			getTime(cam.times.lastLoaded_requestDate, 1);
+		tpl.data['timeLastLoaded'] = getTime(cam.times.lastLoaded, 1);
+		tpl.data['timeLastFailed'] = getTime(cam.times.lastFailed, 1);
+		tpl.data['timeLastAborted'] = getTime(cam.times.lastAborted, 1);
+		tpl.update();
+
+		// XXX: calling update() on templates overwrites sizeWindow(), 
+		// so we must call it again to maintain proper sizing. 
+		this.controller.sizeWindow();
+	}
+
+	/**
+	 * Switch to the all-cameras view.
+	 */
+	this.multiview = function()
+	{
+		$(".singleview_cam").hide();
+		$(".singleview_stats").hide();
+		$(".multiview_cam").show();
+
+		for(var i = 0; i < this.cameras.length; i++) {
+			this.cameras[i].setPause(false);
+		}
+
+		// Heuristic fix for UI 'glitching' out (incorrect col size)
+		this.controller.sizeWindow();
+	}
+
+	/**
+	 * Switch to the single camera view.
+	 * Input: Camera object of the camera we wish to display. 
+	 */
+	this.singleview = function(cam)
+	{
+		var select = this.SINGLEVIEW_IMAGE_SELECTOR + cam.id;
+
+		$(".multiview_cam").hide();
+		$(select).show();
+		$(select + " .singleview_stats").show();
+
+		for(var i = 0; i < this.cameras.length; i++) {
+			if(cam.id == i) {
+				continue;
+			}
+			this.cameras[i].setPause(true);
+		}
+
+		// Heuristic fix for UI 'glitching' out (incorrect col size)
+		this.controller.sizeWindow();
+	}
+
+	/**
+	 * Get the selected row width from the options form.
+	 */
+	this.getOptionRowWidth = function()
+	{
+		//var val = parseInt($('#options option:selected').attr('value'));
+		//return (val > 0 && val < 7)? val : 1;
+                return 1;
+	}
+
+	/**
+	 * Get the selected server (local or remote) from the options form.
+	 */
+	this.getOptionServer = function() 
+	{
+		//var val = $('#options input:checked').attr('value');
+		//return (val == 'local')? 'local' : 'remote';
+                return 'remote';
+	}
+}
